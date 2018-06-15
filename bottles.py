169e2ffc7b6c2705a62063fb25a3fe4607d31406
Jack Spence <jack.c.spence@gmail.com>
2018-06-15 08:28:36 -0400

    Added Random Number Option with basic error handling

--- bottles.py
+++ bottles.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import random
 # make a module to put here, will be used for user input verification
 
 
@@ -11,28 +12,37 @@ def sing(bottles):
     firstLine = "{} bottles of beer on the wall!"
     secondLine = "{} bottles of beer!"
     passIt = "Take one down \nAnd Pass it around"
-
-    print(firstLine.format(bottles))
-    print(secondLine.format(bottles))
-    print(passIt)
-    bottles -= 1        # This is to print 1 less bottle for the last line
     if bottles == 1:    # Checking to see if last bottle on wall
-        print(firstLine.format(bottles).replace("bottles", "bottle"))
-        print()
         print(firstLine.format(bottles).replace("bottles", "bottle"))
         print(secondLine.format(bottles).replace("bottles", "bottle"))
         print(passIt)
         print("No more bottles of beer on the wall!")
-    else:
+    elif bottles > 1:
         print(firstLine.format(bottles))
+        print(secondLine.format(bottles))
+        print(passIt)
+        bottles -= 1        # This is to print 1 less bottle for the last line
+        if bottles == 1:
+            print(firstLine.format(bottles).replace("bottles", "bottle"))
+        else:
+            print(firstLine.format(bottles))
+    else:
+        pass
     print()
 
 
 def main():
     """Main will loop through the sing function 98 times and sing bottles of
     beer on the wall"""
-    wall = 99
-    while wall > 1:     # Loop to decrement number passed to the song
+    choice = input("Enter y to start with a random number(ENTER for 99): ")
+    if choice.lower() == "y":
+        wall = random.randint(1, 99)
+    elif (choice == "") or (choice.lower() == "n"):
+        wall = 99
+    else:
+        print("INVALID CHOICE")
+        return 1
+    while wall >= 1:     # Loop to decrement number passed to the song
         sing(wall)
         wall -= 1
 

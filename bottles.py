318531233b3db5ec9a3c9fdd8369d319e4b6b692
Jack Spence <jack.c.spence@gmail.com>
2018-06-15 12:22:56 -0400

    Added a couple of dictionaries to handle most of the number to word conversion

--- bottles.py
+++ bottles.py
@@ -3,18 +3,29 @@
 import random
 import sys
 import time
-# make a module to put here, will be used for user input verification
 
 
 def sing(bottles, beverage):
     """This function will take an integer and sing however many bottles of
     of beverage needed until there are no more bottles on the wall"""
-
+    tensPlace = {"9": "ninety", "8": "eighty", "7": "seventy", "6": "sixty",
+                 "5": "fifty", "4": "forty", "3": "thirty", "2": "twenty",
+                 "1": "TEEEEEENNN", "0": "DUNNNO"}
+    onePlace = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
+                "6": "six", "7": "seven", "8": "eight", "9": "nine", "0": ""}
     # The lyrics to the songs with format string syntax
     firstLine = "{} bottles of {} on the wall!"
     secondLine = "{} bottles of {}!"
     passIt = "Take one down \nAnd Pass it around"
-    formatTup = (bottles, beverage)
+    bottlesStr = str(bottles)
+    if len(bottlesStr) == 2:
+        wordNum = tensPlace[bottlesStr[0]] + "-" + onePlace[bottlesStr[1]]
+        if bottles % 10 == 0:
+            wordNum = wordNum.replace("-", "")
+        wordNum = wordNum.capitalize()
+        formatTup = (wordNum, beverage)
+    else:
+        formatTup = (onePlace[bottlesStr[0]], beverage)
     if bottles == 1:    # Checking to see if last bottle on wall
         print(firstLine.format(*formatTup).replace("bottles", "bottle"))
         print(secondLine.format(*formatTup).replace("bottles", "bottle"))
@@ -25,7 +36,15 @@ def sing(bottles, beverage):
         print(secondLine.format(*formatTup))
         print(passIt)
         bottles -= 1        # This is to print 1 less bottle for the last line
-        formatTup = (bottles, beverage)
+        bottlesStr = str(bottles)
+        if len(bottlesStr) == 2:
+            wordNum = tensPlace[bottlesStr[0]] + "-" + onePlace[bottlesStr[1]]
+            if bottles % 10 == 0:
+                wordNum = wordNum.replace("-", "")
+            wordNum = wordNum.capitalize()
+            formatTup = (wordNum, beverage)
+        else:
+            formatTup = (onePlace[bottlesStr[0]], beverage)
         if bottles == 1:
             print(firstLine.format(*formatTup).replace("bottles", "bottle"))
         else:
